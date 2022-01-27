2df08a237322822c0114600437d90894eda8dfdd
SrDevO <76437512+SrDaveo28@users.noreply.github.com>
2022-01-27 19:44:16 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Pelicula from './pelicula';
+import { useState } from 'react';
+import PageWrapper from './PageWrapper';
+
+import PeliculasJson from './peliculas.json';
+import Paginacion from './Paginacion';
 
 function App() {
+
+  const [paginaActual, setPaginaActual] = useState(1);
+  /* const [peliculas, setPeliculas] = useState([]); */
+  const TOTAL_POR_PAGINA = 7;
+
+   let peliculas = PeliculasJson;
+
+  /* const buscarPeliculas = async () => {
+    let url = 'https://lucasmoy.dev/data/react/peliculas.json';
+
+    let respuesta = await fetch(url, {
+      "method": 'GET',
+      "mode": 'no-cors',
+      "headers": {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        "Origin": "https://lucasmoy.dev/data/react/peliculas.json"
+      }
+    }); 
+
+    var json = await respuesta.json();
+
+    alert(json);
+    debugger;
+    
+     setPeliculas(json); 
+  }*/
+
+  
+  const cargarPeliculas = () => {
+   peliculas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA, paginaActual * TOTAL_POR_PAGINA);
+  }
+
+  const getTotalPaginas = () => {
+    return Math.ceil(peliculas.length / TOTAL_POR_PAGINA);
+  }
+
+  cargarPeliculas();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <PageWrapper>
+
+      {/* <button onClick={buscarPeliculas}>Pruebas</button> */}
+
+      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+
+        setPaginaActual(pagina);
+
+      }} />
+
+
+      {
+        peliculas.map(pelicula =>
+
+          <Pelicula
+            titulo={pelicula.titulo}
+            calificacion={pelicula.calificacion}
+            director={pelicula.director}
+            actores={pelicula.actores}
+            fecha={pelicula.fecha}
+            duracion={pelicula.duracion}
+            img={pelicula.img}
+          >
+
+            {pelicula.descripcion}
+
+          </Pelicula>
+
+        )
+      }
+
+
+
+
+    </PageWrapper>
+
+
+
+
+
+
+
   );
 }
 
